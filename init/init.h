ed7a4440f9ab112f62b858d6ac2d03f5665b22e4
Nish-droid <59031531+Nish-droid@users.noreply.github.com>
2020-03-08 20:58:17 -0400

    Add files via upload

---
+++ init/init.h
@@ -0,0 +1,20 @@
+/* ------------------------------------------------------------------
+   Robert Nash
+   Joseph
+   Nick
+   Nischay Modi
+
+   University of Maryland, CMSC414, Fall 2019
+
+------------------------------------------------------------------ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define INCORRECT_ARGS  62
+#define FILE_EXISTS     63
+#define PROGRAM_FAILED  64
